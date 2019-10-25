64eaf27a62e83b01839c309ed1f3fbf4641442b8
OlgaKochneva <kochnevaolga74@bk.ru>
2019-10-25 14:44:36 +0300

    Refactoring.Change list to object

--- lab_3/webGL.js
+++ lab_3/webGL.js
@@ -1,5 +1,5 @@
 let gl;
-let figures = [];
+let figures = {};
 let shaderProgram;
 let mvMatrix = mat4.create();
 let pMatrix = mat4.create();
@@ -8,17 +8,18 @@ function webGLStart() {
     const canvas = document.getElementById("central_canvas");
     initGL(canvas);
     initShaders();
-    figures = [
-        new Cube([-5.0, 0.0, 0.0], 0, [5, 1, 5], [0.0, 0.0, 0.0, 1.0]),
-        new Rectangle([0.0, -1.0, 0.0], 90, [100, 1, 100], [0.0, 0.0, 0.0, 1.0]),
-        new Net([5.0, 3.0, 1.0], 0, [8, 8, 1], 15, [128.0/255, 5.0/255, 48/255, 1.0]),
-        new Cylinder([5.0, 2.0, -10.0], 0, [3, 6, 3], [0.0, 0.0, 0.0, 1.0]),
-        new Cube([5.0, 6.0, -10.0], 0, [4, 2, 4], [0.0, 0.0, 0.0, 1.0]),
-        new NetCylinder([-5.0, 5.0, -10.0], 0, [3, 4, 3], 0, 20, [0.0, 0.0, 1.0, 1.0]),
-        new NetCylinder([-5.0, 5.0, -10.0], 0, [3, 4, 3], 6, 0, [0.0, 0.0, 1.0, 1.0]),
-        new NetCylinder([-5.0, 1.0, -10.0], 0, [3, 4, 3], 0, 15, [37/255, 186/255, 65/255, 1.0]),
-        new NetCylinder([-5.0, 1.0, -10.0], 0, [3, 4, 3], 4, 0, [37/255, 186/255, 65/255, 1.0])
-    ];
+    figures = {
+        "cube": new Cube([-5.0, 0.0, 0.0], 0, [5, 1, 5], [0.0, 0.0, 0.0, 1.0]),
+        "scene": new Rectangle([0.0, -1.0, 0.0], 90, [100, 1, 100], [0.0, 0.0, 0.0, 1.0]),
+        "net": new Net([5.0, 3.0, 1.0], 0, [8, 8, 1], 15, [0.5, 0.0, 0.2, 1.0]),
+        "thingCylinder": new Cylinder([5.0, 2.0, -10.0], 0, [3, 6, 3], [0.0, 0.0, 0.0, 1.0]),
+        "thingCube": new Cube([5.0, 6.0, -10.0], 0, [4, 2, 4], [0.0, 0.0, 0.0, 1.0]),
+        "blueNetCylinderLines": new NetCylinder([-5.0, 5.0, -10.0], 0, [3, 4, 3], 0, 20, [0.0, 0.0, 1.0, 1.0]),
+        "blueNetCylinderCircles": new NetCylinder([-5.0, 5.0, -10.0], 0, [3, 4, 3], 6, 0, [0.0, 0.0, 1.0, 1.0]),
+        "greenNetCylinderLines": new NetCylinder([-5.0, 1.0, -10.0], 0, [3, 4, 3], 0, 15, [0.1, 0.7, 0.3, 1.0]),
+        "greenNetCylinderCircles": new NetCylinder([-5.0, 1.0, -10.0], 0, [3, 4, 3], 4, 0, [0.1, 0.7, 0.3, 1.0])
+    };
+
     initBuffers();
     gl.clearColor(0.0, 0.0, 0.0, 0.5);
     gl.lineWidth(3);
@@ -100,8 +101,9 @@ function setMatrixUniforms() {
 }
 
 function initBuffers() {
-    for (let i = 0; i < figures.length; i++) {
-        figures[i].initBuffers();
+    for (let figureName in figures){
+        figures[figureName].initBuffers();
+        alert(figureName);
     }
 }
 
@@ -136,96 +138,88 @@ function drawScene() {
 
     //Draw Scene
     mvPushMatrix();
-    mat4.translate(mvMatrix, figures[1].center);
-    mat4.scale(mvMatrix, figures[1].scale);
-    mat4.rotate(mvMatrix, degToRad(figures[1].angle), [0, 1, 0]);
-    setBuffersToShaders(figures[1].vertexPositionBuffer, figures[1].vertexColorBuffer);
+    mat4.translate(mvMatrix, figures.scene.center);
+    mat4.scale(mvMatrix, figures.scene.scale);
+    mat4.rotate(mvMatrix, degToRad(figures.scene.angle), [0, 1, 0]);
+    setBuffersToShaders(figures.scene.vertexPositionBuffer, figures.scene.vertexColorBuffer);
     setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, figures[1].vertexPositionBuffer.numItems);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, figures.scene.vertexPositionBuffer.numItems);
     mvPopMatrix();
 
     //Draw Cube
     mvPushMatrix();
-    mat4.translate(mvMatrix, figures[0].center); // move view
-    mat4.scale(mvMatrix, figures[0].scale);
-
-    setBuffersToShaders(figures[0].vertexPositionBuffer, figures[0].vertexColorBuffer);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, figures[0].vertexIndexBuffer);
+    mat4.translate(mvMatrix, figures.cube.center); // move view
+    mat4.scale(mvMatrix, figures.cube.scale);
+    setBuffersToShaders(figures.cube.vertexPositionBuffer, figures.cube.vertexColorBuffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, figures.cube.vertexIndexBuffer);
     setMatrixUniforms();
-    gl.drawElements(gl.TRIANGLES, figures[0].vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.TRIANGLES, figures.cube.vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
     mvPopMatrix();
 
-    //Draw Cube2
-    mvPushMatrix();
-    mat4.translate(mvMatrix, figures[4].center); // move view
-    mat4.scale(mvMatrix, figures[4].scale);
+    //Draw strange cube-cylinder thing
 
-    setBuffersToShaders(figures[4].vertexPositionBuffer, figures[4].vertexColorBuffer);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, figures[4].vertexIndexBuffer);
+    mvPushMatrix();
+    mat4.translate(mvMatrix, figures.thingCube.center); // move view
+    mat4.scale(mvMatrix, figures.thingCube.scale);
+    setBuffersToShaders(figures.thingCube.vertexPositionBuffer, figures.thingCube.vertexColorBuffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, figures.thingCube.vertexIndexBuffer);
     setMatrixUniforms();
-    gl.drawElements(gl.TRIANGLES, figures[4].vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.TRIANGLES, figures.thingCube.vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+    mvPopMatrix();
+
+    mvPushMatrix();
+    mat4.translate(mvMatrix, figures.thingCylinder.center);
+    mat4.scale(mvMatrix, figures.thingCylinder.scale);
+    setBuffersToShaders(figures.thingCylinder.vertexPositionBuffer, figures.thingCylinder.vertexColorBuffer);
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, figures.thingCylinder.vertexPositionBuffer.numItems);
     mvPopMatrix();
 
     //Draw Net
-    mvPushMatrix();
-    mat4.translate(mvMatrix, figures[2].center);
-    mat4.scale(mvMatrix, figures[2].scale);
-    //mat4.rotate(mvMatrix, degToRad(-80), [0, 1, 0]);
-    setBuffersToShaders(figures[2].vertexPositionBuffer, figures[2].vertexColorBuffer);
-    setMatrixUniforms();
-    gl.drawArrays(gl.LINES, 0, figures[2].vertexPositionBuffer.numItems);
-    mvPopMatrix();
-
-    //Draw Cylinder
-    mvPushMatrix();
-    mat4.translate(mvMatrix, figures[3].center);
-    mat4.scale(mvMatrix, figures[3].scale);
-    //mat4.rotate(mvMatrix, degToRad(-80), [0, 1, 0]);
-    setBuffersToShaders(figures[3].vertexPositionBuffer, figures[3].vertexColorBuffer);
-    setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, figures[3].vertexPositionBuffer.numItems);
-    mvPopMatrix();
-
-    gl.lineWidth(1);
-    //Draw NetCylinder
-    mvPushMatrix();
-    mat4.translate(mvMatrix, figures[5].center);
-    mat4.scale(mvMatrix, figures[5].scale);
-    //mat4.rotate(mvMatrix, degToRad(-80), [0, 1, 0]);
-    setBuffersToShaders(figures[5].vertexPositionBuffer, figures[5].vertexColorBuffer);
-    setMatrixUniforms();
-    gl.drawArrays(gl.LINES, 0, figures[5].vertexPositionBuffer.numItems);
-    mvPopMatrix();
-
-    //Draw NetCylinder
-    mvPushMatrix();
-    mat4.translate(mvMatrix, figures[6].center);
-    mat4.scale(mvMatrix, figures[6].scale);
-    //mat4.rotate(mvMatrix, degToRad(-80), [0, 1, 0]);
-    setBuffersToShaders(figures[6].vertexPositionBuffer, figures[6].vertexColorBuffer);
-    setMatrixUniforms();
-    gl.drawArrays(gl.LINE_LOOP, 0, figures[6].vertexPositionBuffer.numItems);
-    mvPopMatrix();
-
     gl.lineWidth(3);
-    //Draw NetCylinder
     mvPushMatrix();
-    mat4.translate(mvMatrix, figures[7].center);
-    mat4.scale(mvMatrix, figures[7].scale);
-    //mat4.rotate(mvMatrix, degToRad(-80), [0, 1, 0]);
-    setBuffersToShaders(figures[7].vertexPositionBuffer, figures[7].vertexColorBuffer);
+    mat4.translate(mvMatrix, figures.net.center);
+    mat4.scale(mvMatrix, figures.net.scale);
+    setBuffersToShaders(figures.net.vertexPositionBuffer, figures.net.vertexColorBuffer);
     setMatrixUniforms();
-    gl.drawArrays(gl.LINES, 0, figures[7].vertexPositionBuffer.numItems);
+    gl.drawArrays(gl.LINES, 0, figures.net.vertexPositionBuffer.numItems);
+    mvPopMatrix();
+
+    //Draw blue NetCylinder
+    gl.lineWidth(1);
+    mvPushMatrix();
+    mat4.translate(mvMatrix, figures.blueNetCylinderLines.center);
+    mat4.scale(mvMatrix, figures.blueNetCylinderLines.scale);
+    setBuffersToShaders(figures.blueNetCylinderLines.vertexPositionBuffer, figures.blueNetCylinderLines.vertexColorBuffer);
+    setMatrixUniforms();
+    gl.drawArrays(gl.LINES, 0, figures.blueNetCylinderLines.vertexPositionBuffer.numItems);
+    mvPopMatrix();
+
+    mvPushMatrix();
+    mat4.translate(mvMatrix, figures.blueNetCylinderCircles.center);
+    mat4.scale(mvMatrix, figures.blueNetCylinderCircles.scale);
+    setBuffersToShaders(figures.blueNetCylinderCircles.vertexPositionBuffer, figures.blueNetCylinderCircles.vertexColorBuffer);
+    setMatrixUniforms();
+    gl.drawArrays(gl.LINE_LOOP, 0, figures.blueNetCylinderCircles.vertexPositionBuffer.numItems);
+    mvPopMatrix();
+
+    //Draw green NetCylinder
+    gl.lineWidth(3);
+    mvPushMatrix();
+    mat4.translate(mvMatrix, figures.greenNetCylinderLines.center);
+    mat4.scale(mvMatrix, figures.greenNetCylinderLines.scale);
+    setBuffersToShaders(figures.greenNetCylinderLines.vertexPositionBuffer, figures.greenNetCylinderLines.vertexColorBuffer);
+    setMatrixUniforms();
+    gl.drawArrays(gl.LINES, 0, figures.greenNetCylinderLines.vertexPositionBuffer.numItems);
     mvPopMatrix();
 
     //Draw NetCylinder
     mvPushMatrix();
-    mat4.translate(mvMatrix, figures[8].center);
-    mat4.scale(mvMatrix, figures[8].scale);
-    //mat4.rotate(mvMatrix, degToRad(-80), [0, 1, 0]);
-    setBuffersToShaders(figures[8].vertexPositionBuffer, figures[8].vertexColorBuffer);
+    mat4.translate(mvMatrix, figures.greenNetCylinderCircles.center);
+    mat4.scale(mvMatrix, figures.greenNetCylinderCircles.scale);
+    setBuffersToShaders(figures.greenNetCylinderCircles.vertexPositionBuffer, figures.greenNetCylinderCircles.vertexColorBuffer);
     setMatrixUniforms();
-    gl.drawArrays(gl.LINE_LOOP, 0, figures[8].vertexPositionBuffer.numItems);
+    gl.drawArrays(gl.LINE_LOOP, 0, figures.greenNetCylinderCircles.vertexPositionBuffer.numItems);
     mvPopMatrix();
 
 }
